4c235769a7040ba4d59634a3ba237fc9e6d5dbea
hensetiawan <hendystyawan74@gmail.com>
2023-11-24 10:08:17 +0700

    feat : form validation on iinput income

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,10 +1,16 @@
 import { Row, Col, Modal, Button } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import InputText from "../components/form/InputText";
 import supabase from "../services/supabase";
 function App() {
   const [show, setShow] = useState(false);
   const [income, setExpense] = useState([]);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   const handleRefresh = async () => {
     try {
@@ -18,6 +24,10 @@ function App() {
       console.error(error);
       return error;
     }
+  };
+
+  const onSubmit = async (data) => {
+    console.log(data);
   };
 
   useEffect(() => {
@@ -50,8 +60,7 @@ function App() {
         </Col>
       </Row>
       <Row className="mt-5">
-        <Col lg={10} xs={12}>
-        </Col>
+        <Col lg={10} xs={12}></Col>
       </Row>
 
       {/* modal component*/}
@@ -66,35 +75,63 @@ function App() {
             </label>
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${
+                errors.expenseName ? "border border-danger" : ""
+              }`}
               id="income-name"
               placeholder="income name"
+              {...register("expenseName", { required: true })}
             />
+            {errors.expenseName?.type === "required" && (
+              <p role="alert" className="text-danger mt-2 fs-6 fst-italic">
+                expense name is required
+              </p>
+            )}
             <label htmlFor="income-date" className="form-label mt-2">
               Income Date
             </label>
             <input
               type="date"
-              className="form-control"
+              className={`form-control ${
+                errors.expenseDate ? "border border-danger" : ""
+              }`}
               id="income-date"
               placeholder="income date"
+              {...register("expenseDate", { required: true })}
             />
+            {errors.expenseDate?.type === "required" && (
+              <p role="alert" className="text-danger mt-2 fs-6 fst-italic">
+                expense date is required
+              </p>
+            )}
             <label htmlFor="income-name" className="form-label mt-2">
               Amount Money
             </label>
             <input
               type="number"
-              className="form-control"
+              className={`form-control ${
+                errors.expenseAmount ? "border border-danger" : ""
+              }`}
               id="amount-money"
               placeholder="amount money"
+              {...register("expenseAmount", { required: true })}
             />
+            {errors.expenseAmount?.type === "required" && (
+              <p role="alert" className="text-danger mt-2 fs-6 fst-italic">
+                expense amount is required
+              </p>
+            )}
           </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button type="submit" variant="primary" onClick={handleClose}>
+          <Button
+            type="submit"
+            variant="primary"
+            onClick={handleSubmit(onSubmit)}
+          >
             Create
           </Button>
         </Modal.Footer>
